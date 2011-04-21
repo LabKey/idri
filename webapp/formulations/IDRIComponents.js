36fa00d6a5d2efc867cf627ddf3935e2c79eb65e
Adam Rauch <adamr@labkey.com>
2011-04-21 05:19:47 +0000

    Update licenses

--- webapp/formulations/IDRIComponents.js
+++ webapp/formulations/IDRIComponents.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2011 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 /* Returns an LABKEY.ext.Store of IDRI Formulation temperatures */
 function getTemperatureStore() {
     
--- webapp/formulations/failure/GridQuery.js
+++ webapp/formulations/failure/GridQuery.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2011 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 function queryAssayGrid(assayRowId, formulationName, machine, callbackFn) {
 
     var sql = 'SELECT CASE WHEN psbasepivot.StorageTemperature = \'5C\' THEN \'05C\' ELSE psbasepivot.StorageTemperature END AS Temperature, ' +
