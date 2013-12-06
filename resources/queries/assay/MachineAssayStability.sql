9797a7f6de820ee65bd20d8c3c50bf50161265cd
Adam Rauch <adamr@labkey.com>
2013-12-06 21:37:11 +0000

    Update licenses

--- resources/queries/assay/MachineAssayStability.sql
+++ resources/queries/assay/MachineAssayStability.sql
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2013 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 PARAMETERS (AssayRowId INTEGER, MachineType VARCHAR)
 SELECT CASE WHEN pbp.StorageTemperature = '5C' THEN '05C' ELSE pbp.StorageTemperature END AS Temperature,
 MIN(pbp."T=0") AS "DM",
