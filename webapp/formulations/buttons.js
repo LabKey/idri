b30493c25a0518af311830c975fac2f4eae73361
Adam Rauch <adamr@labkey.com>
2014-02-06 16:48:55 +0000

    Update licenses

--- webapp/formulations/buttons.js
+++ webapp/formulations/buttons.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2014 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0: http://www.apache.org/licenses/LICENSE-2.0
+ */
 /**
  * Created by Nick Arnold on 1/21/14.
  */
