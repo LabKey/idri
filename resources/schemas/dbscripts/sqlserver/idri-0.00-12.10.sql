e083cd046949ddb42497edf7dcddf753506bd1df
Adam Rauch <adamr@labkey.com>
2014-03-10 00:28:42 +0000

    Upgrade only from 12.10 and beyond (not 11.30), per our upgrade support policy
    Consolidate all bootstrap scripts to 0.00-12.10. Obsolete unreachable scripts and incremental scripts from 13.3.
    
    Remove no longer needed StudyUpgradeCode.migrateReportProperties()

---
+++ resources/schemas/dbscripts/sqlserver/idri-0.00-12.10.sql
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2011-2012 LabKey Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/* idri-0.00-11.10.sql */
+
+CREATE SCHEMA idri;
+GO
+
+CREATE TABLE idri.concentrations
+(
+	RowId INT IDENTITY(1, 1) NOT NULL,
+	Concentration FLOAT NOT NULL,
+	Unit NVARCHAR(255) NOT NULL,
+	Compound INT NOT NULL,
+	Material INT NOT NULL,
+	Lot INT NOT NULL,
+	IsTop BIT,
+
+	CONSTRAINT PK_Constraints PRIMARY KEY (RowID),
+	CONSTRAINT FK_Compounds FOREIGN KEY (Compound) REFERENCES exp.Material(RowId),
+	CONSTRAINT FK_Materials FOREIGN KEY (Material) REFERENCES exp.Material(RowId),
+	CONSTRAINT FK_Lot FOREIGN KEY (Lot) REFERENCES exp.Material(RowId)
+);
+
+/* idri-11.30-12.10.sql */
+
+ALTER TABLE idri.concentrations ADD Container ENTITYID null
+GO
+
+UPDATE idri.concentrations SET Container = (SELECT Container FROM exp.Material r WHERE r.RowId = Lot)
+GO
+
+ALTER TABLE idri.concentrations ALTER COLUMN Container ENTITYID NOT NULL
+GO
