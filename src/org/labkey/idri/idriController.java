acd8458a5be1a2b435a31de3132edcb114ad1936
Karl Lum <klum@labkey.com>
2020-01-02 15:49:56 -0800

    ensure page titles (#39)

--- src/org/labkey/idri/idriController.java
+++ src/org/labkey/idri/idriController.java
@@ -183,7 +183,7 @@ public class idriController extends SpringActionController
         @Override
         public NavTree appendNavTrail(NavTree root)
         {
-            return root;
+            return root.addChild("Create Formulation");
         }
     }
 
